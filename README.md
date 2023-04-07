bf1ed77d12b8efea9f65844b9e2a892e18883cc7
Lucas Favaro Borsatto <paradigma0621@gmail.com>
2023-04-07 17:35:25 -0300

    Small changes

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ Runs on port 8765.
 2023-04-07 - Unified Config + Eureka + Report + Gateway (See specific branch inside all their repositories: "2023-04-07-Config+Eureka+Report+Gateway"). For a test, up the microservices report-java8 and access the endpoint:
 	http://localhost:8765/persons/getPersonMsg
 
+List of microservices correlated: <br>
+https://github.com/paradigma0621/timeflowsystem-config-java8 <br>
+https://github.com/paradigma0621/timeflowsystem-eureka-java8 <br>
+https://github.com/paradigma0621/timeflowsystem-report-java8 <br>
+https://github.com/paradigma0621/timeflowsystem-core-java8 <br>
+https://github.com/paradigma0621/timeflowsystem-api-java8 <br>
+https://github.com/paradigma0621/timeflowsystem-gateway-java8
+
+In https://github.com/paradigma0621 there is algo all this microservices in Java 17 version.
--- src/main/java/com/timeflow/gateway/configuration/GatewayConfiguration.java
+++ src/main/java/com/timeflow/gateway/configuration/GatewayConfiguration.java
@@ -11,19 +11,17 @@ public class GatewayConfiguration {
 	@Bean
 	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
 		return builder.routes()
-                 .route(p -> p.path("/get")
+				.route(p -> p.path("/get")
                                  .filters(f -> f
                                          .addRequestHeader("Hello", "World")     //Chamada site com header
                                          .addRequestParameter("Hello", "World")) //e aqui, junto nessa chamada, passa parâmetros
-                         .uri("http://httpbin.org:80"))
-         .route(p -> p.path("/api-java17/**")
-                         .uri("lb://api-java17"))
-         .route(p -> p.path("/persons/**")
-                         .uri("lb://report-java8"))
-		.route(p -> p.path("/theEndpoint/**")
-		.uri("lb://report-java8"))
-
-         .build();
-
+					.uri("http://httpbin.org:80"))
+				.route(p -> p.path("/somewhere/**")
+					.uri("lb://api-java8"))
+				.route(p -> p.path("/persons/**")
+					.uri("lb://report-java8"))
+				.route(p -> p.path("/theEndpoint/**")
+					.uri("lb://report-java8"))
+				.build();
 	}
 }
